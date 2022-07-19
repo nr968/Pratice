04bca31a600bf59dd45ddc0421775c7529727f97
nr968 <nrnaveenkmr@gmail.com>
2022-07-19 13:27:57 +0530

    Refractored the code

--- JavaScript/05-Guess-My-Number/Guess My Number/script.js
+++ JavaScript/05-Guess-My-Number/Guess My Number/script.js
@@ -1,57 +1,71 @@
 "use strict";
 
-let random = Math.trunc(Math.random() * 20) + 1;
-console.log(random);
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
+
+function setTextContent(cssClass, text) {
+  document.querySelector(cssClass).textContent = text;
+}
 
 document.querySelector(".check-btn").addEventListener("click", function () {
   const guessedNumber = Number(document.querySelector(".numberInput").value);
 
+  //if guessed number is 0
   if (!guessedNumber) {
-    document.querySelector(".startGuess").textContent = "⛔️ Not a number...";
-  } else if (guessedNumber > 20) {
-    document.querySelector(".startGuess").textContent =
-      "⛔️ Number hign than 20...";
-  } else if (random !== guessedNumber) {
-    if (score === 1) {
-      document.querySelector(".startGuess").textContent =
-        "💥 You Lost the Gamee";
-      document.querySelector(".score-number").textContent = 0;
-    } else {
-      score--;
-      document.querySelector(".score-number").textContent = String(score);
+    setTextContent(".startGuess", "⛔️ Not a number...");
+  }
 
-      if (guessedNumber > random) {
-        document.querySelector(".startGuess").textContent = "📈 Too high...";
-      } else if (guessedNumber < random) {
-        document.querySelector(".startGuess").textContent = "📉 Too Low...";
+  //if guessed number is >20
+  else if (guessedNumber > 20) {
+    setTextContent(".startGuess", "⛔️ Number hign than 20...");
+  }
+
+  //if guessed number between 1-20
+  else {
+    //if guessed number != secertNumber
+    if (secretNumber !== guessedNumber) {
+      if (score === 1) {
+        setTextContent(".startGuess", "💥 You Lost the Gamee");
+        setTextContent(".score-number", 0);
+      } else {
+        score--;
+        setTextContent(".score-number", score);
+        setTextContent(
+          ".startGuess",
+          guessedNumber > secretNumber ? "📈 Too high..." : "📉 Too Low..."
+        );
       }
     }
-  } else if (random === guessedNumber) {
-    highScore = Number(document.querySelector(".highscore-number").textContent);
 
-    if (score > highScore) {
-      highScore = score;
-      document.querySelector(".highscore-number").textContent = String(score);
+    //if guessed number === secertNumber
+    else if (secretNumber === guessedNumber) {
+      highScore = Number(
+        document.querySelector(".highscore-number").textContent
+      );
+
+      if (score > highScore) {
+        highScore = score;
+        setTextContent(".highscore-number", highScore);
+      }
+
+      setTextContent(".guesss", secretNumber);
+      setTextContent(".startGuess", "Correct Number🎉🎉🎉");
+      document.querySelector("body").style.backgroundColor = "#60b347";
+      document.querySelector(".guesss").style.width = "30rem";
     }
-
-    document.querySelector(".guesss").textContent = String(random);
-    document.querySelector(".startGuess").textContent = "Correct Number🎉🎉🎉";
-    document.querySelector(".hint").textContent = "";
-    document.querySelector("body").style.backgroundColor = "#60b347";
-    document.querySelector(".guesss").style.width = "30rem";
   }
 });
 
+//To play the Game Again
 document.querySelector(".again").addEventListener("click", function () {
   document.querySelector("body").style.backgroundColor = "#222";
   document.querySelector(".guesss").style.width = "15rem";
-  document.querySelector(".guesss").textContent = "?";
+  setTextContent(".guesss", "?");
   document.querySelector(".numberInput").value = "";
-  document.querySelector(".startGuess").textContent = "Start Guessing...";
+  setTextContent(".startGuess", "Start Guessing...");
   score = 20;
   document.querySelector(".score-number").textContent = score;
-  random = Math.trunc(Math.random() * 20) + 1;
-  console.log(random);
+  setTextContent(".score-number", score);
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
 });
